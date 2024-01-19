5978c6b9028e53b10abceb24e49fe57999d71f1a
E-KhangalN <ehangal525@gmail.com>
2024-01-19 10:39:11 +0800

    б

--- app/home/about.js
+++ app/home/about.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+const proImg = require("../../assets/iimg/pro.jpg");
 const About = () => {
   return (
     <View>
-      <Text>Миний тухай</Text>
+      {/* Толгой хэсэг */}
+      <View style={styles.header}>
+        <Image source={proImg} style={styles.proImg} />
+        <View style={styles.followers}>
+          <View style={styles.followersItem}>
+            <Text style={styles.followersText}> 6 </Text>
+            <Text style={styles.followersText}> Post </Text>
+          </View>
+          <View style={styles.followersItem}>
+            <Text style={styles.followersText}> 147 </Text>
+            <Text style={styles.followersText}> Followers </Text>
+          </View>
+          <View style={styles.followersItem}>
+            <Text style={styles.followersText}> 634 </Text>
+            <Text style={styles.followersText}> Following </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default About;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  proImg: {
+    width: 107,
+    height: 107,
+    borderRadius: "50%",
+    flex: 1,
+  },
+  followers: {
+    flex: 3,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  followersText: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  followersItem: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
