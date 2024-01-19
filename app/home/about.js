79823327bec5ee1f007673c186efd4af049c598e
E-KhangalN <ehangal525@gmail.com>
2024-01-19 21:34:02 +0800

    u

--- app/home/about.js
+++ app/home/about.js
@@ -4,54 +4,50 @@ import PagerView from "react-native-pager-view";
 const proImg = require("../../assets/iimg/pro.jpg");
 const About = () => {
   return (
-    <View>
-      <View style={styles.container}>
-        {/* Толгой хэсэг */}
-        <View style={styles.header}>
-          <Image source={proImg} style={styles.proImg} />
-          <View style={styles.followers}>
-            <View style={styles.followersItem}>
-              <Text style={styles.followersText}> 6 </Text>
-              <Text style={styles.followersText}> Post </Text>
-            </View>
-            <View style={styles.followersItem}>
-              <Text style={styles.followersText}> 147 </Text>
-              <Text style={styles.followersText}> Followers </Text>
-            </View>
-            <View style={styles.followersItem}>
-              <Text style={styles.followersText}> 634 </Text>
-              <Text style={styles.followersText}> Following </Text>
-            </View>
+    <View style={styles.container}>
+      {/* Толгой хэсэг */}
+      <View style={styles.header}>
+        <Image source={proImg} style={styles.proImg} />
+        <View style={styles.followers}>
+          <View style={styles.followersItem}>
+            <Text style={styles.followersText}> 6 </Text>
+            <Text style={styles.followersText}> Post </Text>
+          </View>
+          <View style={styles.followersItem}>
+            <Text style={styles.followersText}> 147 </Text>
+            <Text style={styles.followersText}> Followers </Text>
+          </View>
+          <View style={styles.followersItem}>
+            <Text style={styles.followersText}> 634 </Text>
+            <Text style={styles.followersText}> Following </Text>
           </View>
         </View>
-        {/* bio */}
-        <View style={styles.bio}>
-          <Text style={styles.bioName}>Md Sazid</Text>
-          <Text style={styles.bioDesc}>
-            insta chalai na temon -.- Btw Hi :V
-          </Text>
-          <Text style={styles.bioDesc}>Byeeeeeeeeeee.</Text>
-        </View>
-        {/* Товч */}
-        <View style={styles.edit}>
-          <TouchableOpacity style={styles.editbtn}>
-            <Text style={styles.editText}> Edit Profile </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.editbtn}>
-            <Text style={styles.editText}> Хичээлүүд </Text>
-          </TouchableOpacity>
-        </View>
-        {/* Tab pages */}
-        <PagerView style={styles.viewPager} initialPage={0}>
-          <View style={styles.page} key="1">
-            <Text>gg</Text>
-            <Text>Swipe ➡️</Text>
-          </View>
-          <View style={styles.page} key="2">
-            <Text>Second page</Text>
-          </View>
-        </PagerView>
       </View>
+      {/* bio */}
+      <View style={styles.bio}>
+        <Text style={styles.bioName}>Md Sazid</Text>
+        <Text style={styles.bioDesc}>insta chalai na temon -.- Btw Hi :V</Text>
+        <Text style={styles.bioDesc}>Byeeeeeeeeeee.</Text>
+      </View>
+      {/* Товч */}
+      <View style={styles.edit}>
+        <TouchableOpacity style={styles.editbtn}>
+          <Text style={styles.editText}> Edit Profile </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.editbtn}>
+          <Text style={styles.editText}> Хичээлүүд </Text>
+        </TouchableOpacity>
+      </View>
+      {/* Tab pages */}
+      <PagerView style={styles.viewPager} initialPage={0}>
+        <View style={styles.page} key="1">
+          <Text> First page </Text>
+          <Text>Swipe ➡️</Text>
+        </View>
+        <View style={styles.page} key="2">
+          <Text>Second page</Text>
+        </View>
+      </PagerView>
     </View>
   );
 };
