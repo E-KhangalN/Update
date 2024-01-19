a9a2b831277e823e7951ffb162223036e5f0124b
E-KhangalN <ehangal525@gmail.com>
2024-01-19 11:06:32 +0800

    L

--- app/home/about.js
+++ app/home/about.js
@@ -1,32 +1,45 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 const proImg = require("../../assets/iimg/pro.jpg");
 const About = () => {
   return (
     <View>
-      {/* Толгой хэсэг */}
-      <View style={styles.header}>
-        <Image source={proImg} style={styles.proImg} />
-        <View style={styles.followers}>
-          <View style={styles.followersItem}>
-            <Text style={styles.followersText}> 6 </Text>
-            <Text style={styles.followersText}> Post </Text>
-          </View>
-          <View style={styles.followersItem}>
-            <Text style={styles.followersText}> 147 </Text>
-            <Text style={styles.followersText}> Followers </Text>
-          </View>
-          <View style={styles.followersItem}>
-            <Text style={styles.followersText}> 634 </Text>
-            <Text style={styles.followersText}> Following </Text>
+      <View style={styles.container}>
+        {/* Толгой хэсэг */}
+        <View style={styles.header}>
+          <Image source={proImg} style={styles.proImg} />
+          <View style={styles.followers}>
+            <View style={styles.followersItem}>
+              <Text style={styles.followersText}> 6 </Text>
+              <Text style={styles.followersText}> Post </Text>
+            </View>
+            <View style={styles.followersItem}>
+              <Text style={styles.followersText}> 147 </Text>
+              <Text style={styles.followersText}> Followers </Text>
+            </View>
+            <View style={styles.followersItem}>
+              <Text style={styles.followersText}> 634 </Text>
+              <Text style={styles.followersText}> Following </Text>
+            </View>
           </View>
         </View>
-      </View>
-      {/* bio */}
-      <View style={styles.bio}>
-        <Text style={styles.bioName}>Md Sazid</Text>
-        <Text style={styles.bioDesc}>insta chalai na temon -.- Btw Hi :V</Text>
-        <Text style={styles.bioDesc}>Byeeeeeeeeeee.</Text>
+        {/* bio */}
+        <View style={styles.bio}>
+          <Text style={styles.bioName}>Md Sazid</Text>
+          <Text style={styles.bioDesc}>
+            insta chalai na temon -.- Btw Hi :V
+          </Text>
+          <Text style={styles.bioDesc}>Byeeeeeeeeeee.</Text>
+        </View>
+        {/* Товч */}
+        <View style={styles.edit}>
+          <TouchableOpacity style={styles.editbtn}>
+            <Text style={styles.editText}> </Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.editbtn}>
+          <Text style={styles.editText}> </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -58,12 +71,32 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  bio: {},
+  bio: {
+    marginBottom: 8,
+  },
   bioName: {
     fontSize: 20,
     fontWeight: "bold",
   },
   bioDesc: {
     fontSize: 20,
+  },
+  editbtn: {
+    borderWidth: 1,
+    paddingVertical: 8,
+    borderRadius: 8,
+    flex: 1,
+    alignItems: "center",
+    marginHorizontal: 5,
+  },
+  editText: {
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  edit: {
+    flexDirection: "row",
+  },
+  container: {
+    padding: 8,
   },
 });
