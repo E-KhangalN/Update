0a2479446de0dcdb4fb5e1fcffdc09fd575bb933
E-KhangalN <ehangal525@gmail.com>
2024-02-03 19:27:44 +0800

    updne

--- app/directmessage/index.js
+++ app/directmessage/index.js
@@ -1,8 +1,9 @@
 // Messenjer лүү ороход шууд харагддаг хэсэг буюу нүүр хуудасын хэсэг энд байрлана. Үүнийг харуулахыг индэкс дотроо бичнэ. Энэ. Товчлол нь rnfes.
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "expo-router";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import PagerView from "react-native-pager-view";
 // Chat screen
 const Dm = () => {
   {
@@ -14,43 +15,105 @@ const Dm = () => {
   }
   const navigation =
     useNavigation(); /*энэ мөрний код хуудас шилжих код. Яг дор байрлах код хуудас шилжээд ороод ирэхэд ажиллана уг кодыг дотор нь тайлбарлая.*/
-  // navigation буюу хуудас өөрчлөгдөх бүрт доторхи кодыг ажиллуулна.
+  const Posts = ({ data }) => {
+    const [selectedTab, setSetselectedTab] = useState(0);
+    const onPageChange = (data) => {
+      const pageNumber = data.nativeEvent.position;
+      console.log(pageNumber);
+      setSetselectedTab(pageNumber);
+    };
+    // navigation буюу хуудас өөрчлөгдөх бүрт доторхи кодыг ажиллуулна.
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerShown: true,
-      headerBackTitleVisible: true,
-      headerBackTitle: "Direct",
-      headerTitle: "",
-      headerRight: () => (
-        <View style={styles.row}>
-          <TouchableOpacity>
-            <AntDesign name="videocamera" size={24} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome name="pencil-square-o" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-      ),
-    }); /* энэ код нь эхлээд navigation -ийнхээ options -ийг тохируулж байгаад таслалын урд 
+    useEffect(() => {
+      navigation.setOptions({
+        headerShown: true,
+        headerBackTitleVisible: true,
+        headerBackTitle: "Direct",
+        headerTitle: "",
+        headerRight: () => (
+          <View style={styles.row}>
+            <TouchableOpacity>
+              <AntDesign name="videocamera" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <FontAwesome name="pencil-square-o" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+        ),
+      }); /* энэ код нь эхлээд navigation -ийнхээ options -ийг тохируулж байгаад таслалын урд 
     талынх буюу headerShown: true энэ нь дэлгэцийн дээд талын хэсгийг харуул гэж байгаа билээ. headerBackTitleVisible: true нь ард талын backtitle -ийг нь харуул
     гэж байна. Яг үүний дараагийн үйлдэл нь backtitle -ийн нэрийг солих үйл явц явагдаж байна. Нэрийг солихдоо headerBackTitle:" өгөх нэрээ энд бичнэ " гэж солино. 
     Харин дээд хэсгийн голд харагддаг бичгийг солихдоо headerTitle:" өгөх нэрээ энд бичнэ " гэж солино. Гэвч хийх гэж байгаа зүйл дээр маань тийм нэр байхгүй учир тийм нэр сольхоос 
     өмнө анх файлын нэрээр орж ирдэг тэрийг байхгүй болгохын тулд нэр өгөх хэсгээ хоосон үлдээчихвэл юм үл үзэгдэнэ. Гол нь срингээ наах хэрэгтэй тэгэхгүй бол алдаа заана. 
     Харин одоо баруун талд нь байрлах icon-уудыг оруулья. icon -oo олохдоо мөн л гүүглээсээ expo icon гэж хайна. Оруулахдаа
      headerRight: () => ( <View> энд оруулах зүйлээ бичиж өгнө </View> )   */
-  }, [navigation]);
-  return (
-    <View>
-      <Text> fhfks;ldmfdljg;dlgd</Text>
-    </View>
-  );
-};
+    }, [navigation]);
+    return (
+      <View>
+        <PagerView
+          onPageSelected={onPageChange}
+          style={styles.pagerView}
+          initialPage={0}
+        >
+          {/* Zuragtai huudas */}
+          <View key="1" style={styles.postContainer}>
+            {data.map((post) => (
+              <Link href={"/post/" + post.id} asChild>
+                <TouchableOpacity style={styles.imgBtn}>
+                  <Image source={{ uri: post.img }} style={styles.image} />
+                </TouchableOpacity>
+              </Link>
+            ))}
+          </View>
 
+          <View key="2">
+            <Text>Second page</Text>
+          </View>
+        </PagerView>{" "}
+      </View>
+    );
+  };
+};
 export default Dm;
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: "row" /* чиглэл нь хэвтээ байх ёстой. */,
     gap: 8 /* дотор нь байгаа элементүүдийн дотор нь зай авч байгаа */,
+  },
+  imgBtn: {
+    width: "32%",
+    aspectRatio: 1,
+    height: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  postContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    gap: 3,
+    flexWrap: "wrap",
+  },
+  btn: {
+    flex: 1,
+    alignItems: "center",
+    paddingBottom: 16,
+    paddingTop: 14,
+  },
+  active: {
+    borderBottomWidth: 2,
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  img: { backgroundColor: "black" },
+  container: {
+    flex: 1,
+  },
+  pagerView: {
+    flex: 1,
   },
 });
